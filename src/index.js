9860ff13c5d680bd588e7f8c9be03f134c632e3e
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-05-06 17:25:31 +0600

    added a sample component to render pizza details.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import ReactDOM from "react-dom/client"
 import React from "react"
+import {pizzaData} from "./data";
+
 
 const App = () => {
-    return <h1>Hello World!</h1>
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <Pizza/>
+        </>
+    )
+}
+
+const Pizza = () => {
+    return <h2>Pizza</h2>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
