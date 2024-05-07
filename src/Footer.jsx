f437208da96e2004685b57c237862ad6cca1a7e2
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-05-07 06:57:10 +0600

    updated the content of Footer to display when the shop is open and when it's closed.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,18 @@
 export const Footer = () => {
-    return (<footer>
-        {new Date().toLocaleTimeString()}. We're currently open
-    </footer>);
+
+    const hour = new Date().getHours();
+    const openingHour = 11;
+    const closingHour = 23;
+    const isOpen = hour >= openingHour && hour <= openingHour;
+    if (isOpen) {
+        return (<footer>
+            {new Date().toLocaleTimeString()}. We're currently open
+        </footer>);
+    } else {
+        return (<footer>
+            Sorry, we're currently closed
+        </footer>);
+    }
+
+
 }
