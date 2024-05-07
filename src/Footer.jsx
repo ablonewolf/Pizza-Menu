fdaf5f95693c74b794efa29735e8f79cc6269af9
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-05-07 07:07:44 +0600

    fixed the logic of whether the shop is open or not.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     const hour = new Date().getHours();
     const openingHour = 11;
     const closingHour = 23;
-    const isOpen = hour >= openingHour && hour <= openingHour;
+    const isOpen = hour >= openingHour && hour <= closingHour;
     if (isOpen) {
         return (<footer>
             {new Date().toLocaleTimeString()}. We're currently open
